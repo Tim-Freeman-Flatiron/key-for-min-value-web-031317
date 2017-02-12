97dbcc34d79526e27d20aac54baa16a1b4c1de45
Tim Freeman <d.timothy.freeman@gmail.com>
2017-02-12 00:05:43 -0500

    Key For Min Value -- Passed Tests

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+	min_key =
+	values = []
+	compare =
+	if name_hash.size == 0
+		min_key = nil
+	else
+	name_hash.each do |key, value|
+		values << value
+		end
+		compare = values[0]
+		values.each do |value|
+			if value < compare
+				compare = value
+			end
+		min_key = name_hash.key(compare)
+	end
 
+	end
+	min_key
 end
